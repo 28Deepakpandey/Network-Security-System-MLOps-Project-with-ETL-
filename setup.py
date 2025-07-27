52dd4992895e639e224803f9a5e8e8fd93054599
28Deepakpandey <Deepakpandey28july@gmail.com>
2025-07-27 08:59:30 +0530

    Exception handling and logging completed

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     name = "NetworkSecurity",
     version = "0.0.1",
     author = "Deepak Pandey",
-    author_emai="deepakpandey28july@gmail.com",
+    author_email="deepakpandey28july@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements()
 )
