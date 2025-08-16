e6ae518514be7fd2c0516ba7d5047d9b3b0e0bf5
28Deepakpandey <Deepakpandey28july@gmail.com>
2025-08-16 18:27:48 +0530

    Data Validation completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -15,13 +15,31 @@ FILE_NAME: str = "phisingData.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
+
+
 """
 Data Ingestion related constants
 (All constants start with DATA_INGESTION_)
 """
+
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME: str = "DEEPAKAI"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
+
+"""
+Data Validation related constant start with DATA_VALIDATION var name
+"""
+DATA_VALIDATION_DIR_NAME:str = "Data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
